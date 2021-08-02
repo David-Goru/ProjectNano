38de74386e771f8bfaaaa70033ce8d90cfc9554f
David Gómez <david.gomez.rooiz@gmail.com>
2021-08-02 18:19:53 +0200

    Deployment test

--- projectnano/src/App.js
+++ projectnano/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Work In Progress
+          Work In Progress!
         </p>
       </header>
     </div>
