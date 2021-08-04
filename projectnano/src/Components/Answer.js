156cae3ab0a4a304ac39336469ef9c9c8a83c628
David Gómez <david.gomez.rooiz@gmail.com>
2021-08-04 10:02:38 +0200

    Added previous and next question buttons

--- projectnano/src/Components/Answer.js
+++ projectnano/src/Components/Answer.js
@@ -5,5 +5,5 @@ export function Answer({ answer }) {
     const [selected, updateSelected] = useState(false);
     const selectionClass = selected ? (answer.valid ? "correct" : "incorrect") : "unselected";
 
-    return <div onClick={() => updateSelected(true)} className={"answer " + selectionClass}> {answer.text}</div>;
+    return <div onClick={() => updateSelected(true)} className={"answer answer-" + selectionClass}> {answer.text}</div>;
 }
--- projectnano/src/Components/App.js
+++ projectnano/src/Components/App.js
@@ -11,6 +11,16 @@ function App() {
             <header className="App-header">
                 <Title text="Project Nano" />
                 <Question question={defaultQuestions[currentQuestion]} index={currentQuestion} />
+                <div className="App-buttons">
+                    <button className="App-button" onClick={() => {
+                        if (currentQuestion === 0) updateCurrentQuestion(defaultQuestions.length - 1);
+                        else updateCurrentQuestion(currentQuestion - 1);
+                    }}> Previous Question </button>
+                    <button className="App-button" onClick={() => {
+                        if (currentQuestion === (defaultQuestions.length - 1)) updateCurrentQuestion(0);
+                        else updateCurrentQuestion(currentQuestion + 1);
+                    }}> Next Question </button>
+                </div>
             </header>
         </div>
     );
