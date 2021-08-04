7d3b3f504e00724d821e91e2d3180ec39351602c
David Gómez <david.gomez.rooiz@gmail.com>
2021-08-04 10:10:35 +0200

    Added array shuffle function + implemented it to answers array

--- projectnano/src/Components/Answers.js
+++ projectnano/src/Components/Answers.js
@@ -1,6 +1,8 @@
 ﻿import { Answer } from "./Answer";
+import { shuffle } from "../Services/ArrayTools";
 
 export function Answers({ answers, questionIndex }) {
+    answers = shuffle(answers);
     return answers.map((answer, index) =>
         <Answer
             key={questionIndex + "#" + index}
---
+++ projectnano/src/Services/ArrayTools.js
@@ -0,0 +1,12 @@
+﻿export function shuffle(array) {
+    var currentIndex = array.length;
+
+    while (0 !== currentIndex) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
